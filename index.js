58bde60af649b4393ebdff589b5e2c80ceaee56b
david@ <mijkm805@gmail.com>
2024-01-21 09:30:05 -0500

    update index.html/js style.css

--- index.js
+++ index.js
@@ -31,3 +31,83 @@ function disableDarkMode() {
   body.classList.remove("lightmode");
 }
 
+
+/*about page text scrumble*/
+class TextScramble {
+  constructor(el) {
+      this.el = el;
+      this.chars = '!<>-_\\/[]{}—=+*^?#________';
+      this.update = this.update.bind(this);
+  }
+
+  setText(newText) {
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise((resolve) => this.resolve = resolve);
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+          const from = oldText[i] || '';
+          const to = newText[i] || '';
+          const start = Math.floor(Math.random() * 40);
+          const end = start + Math.floor(Math.random() * 40);
+          this.queue.push({ from, to, start, end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+  }
+
+  update() {
+      let output = '';
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+          let { from, to, start, end, char } = this.queue[i];
+          if (this.frame >= end) {
+              complete++;
+              output += to;
+          } else if (this.frame >= start) {
+              if (!char || Math.random() < 0.28) {
+                  char = this.randomChar();
+                  this.queue[i].char = char;
+              }
+              output += `<span class="dud">${char}</span>`;
+          } else {
+              output += from;
+          }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+          this.resolve();
+      } else {
+          this.frameRequest = requestAnimationFrame(this.update);
+          this.frame++;
+      }
+  }
+
+  randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+const fx = new TextScramble(document.querySelector('.text'));
+
+const phrases = [
+  'THORNGDAVID | TD',
+  'AS',
+  'A FRONTEND DEVELOPER !',
+  'A BACKEND DEVELOPER !',
+  'A UI DESIGNER !',
+  'LEARN MORE ABOUT ME ABOVE...'
+];
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 4000);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
+
